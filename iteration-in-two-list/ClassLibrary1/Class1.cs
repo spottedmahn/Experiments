5d45cfcce89f104647d35ca25208754bac5723a5
Mike DePouw <mdepouw@ceiamerica.com>
2017-11-10 18:24:20 -0500

    minor refactoring

--- iteration-in-two-list/ClassLibrary1/Class1.cs
+++ iteration-in-two-list/ClassLibrary1/Class1.cs
@@ -7,27 +7,27 @@ namespace ClassLibrary1
 {
     public class Class1
     {
-        public static List<Data> GetResults(List<Data> listTest1, List<Data> listTest2)
+        public static List<Data> GetResults(List<Data> list1, List<Data> list2)
         {
-            var joined = listTest1.FullJoin(listTest2, key1 => key1.Abbrv,
+            var joined = list1.FullJoin(list2, key1 => key1.Abbrv,
                 firstSelector => new Data
                 {
                     Abbrv = firstSelector.Abbrv,
                     Date = firstSelector.Date,
                     Desc = firstSelector.Desc
                 },
-            secondSelector => new Data
-            {
-                Abbrv = secondSelector.Abbrv,
-                Date = secondSelector.Date,
-                Desc = secondSelector.Desc
-            },
-            (first, second) => new Data
-            {
-                Abbrv = second.Abbrv,
-                Date = second.Date,
-                Desc = second.Desc
-            });
+                secondSelector => new Data
+                {
+                    Abbrv = secondSelector.Abbrv,
+                    Date = secondSelector.Date,
+                    Desc = secondSelector.Desc
+                },
+                (first, second) => new Data
+                {
+                    Abbrv = second.Abbrv,
+                    Date = second.Date,
+                    Desc = second.Desc
+                });
 
             var mem = joined.ToList();
 
