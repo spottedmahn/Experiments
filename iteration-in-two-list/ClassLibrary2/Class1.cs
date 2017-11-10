b9338bb191761a2bb951a665a0bb7fb580bbb96b
Mike DePouw <mdepouw@ceiamerica.com>
2017-11-10 18:15:20 -0500

    init commit

---
+++ iteration-in-two-list/ClassLibrary2/Class1.cs
@@ -0,0 +1,50 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary2
+{
+    /// <summary>
+    /// https://stackoverflow.com/questions/47222499/iteration-in-two-list
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// If the abbrv and the date is the same I am adding the object from listTest2 to listTest3
+        /// If the abbrv is the same but the date is different I am adding the object from listTest2 to listTest3 and I am switching the completed property to true.  In addition, I am adding the record from listTest1 to listTest3.
+        /// If the abbrv from listTest1 doesn't exist in listTest2, I am adding the record from listTest1 to listTest3
+        /// If the abbrv from listTest2 doesn't exist in listTest1, I am adding the record from listTest2 to listTest3
+        /// </summary>
+        /// <param name="listTest1"></param>
+        /// <param name="listTest2"></param>
+        /// <returns></returns>
+        public static List<Data> GetResults(List<Data> listTest1, List<Data> listTest2)
+        {
+            //https://stackoverflow.com/questions/5489987/linq-full-outer-join
+            var joined = listTest1.FullJoin(listTest2, key1 => key1.Abbrv,
+                firstSelector => new Data
+                {
+                    Abbrv = firstSelector.Abbrv,
+                    Date = firstSelector.Date,
+                    Desc = firstSelector.Desc
+                },
+            secondSelector => new Data
+            {
+                Abbrv = secondSelector.Abbrv,
+                Date = secondSelector.Date,
+                Desc = secondSelector.Desc
+            },
+            (first, second) => new Data
+            {
+                Abbrv = second.Abbrv,
+                Date = second.Date,
+                Desc = second.Desc
+            });
+
+            var mem = joined.ToList();
+
+            return mem;
+        }
+    }
+}
---
+++ iteration-in-two-list/ClassLibrary2/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Data
+    {
+        public string Abbrv { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Desc { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is Data data
+                && Abbrv == data.Abbrv)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/371328/why-is-it-important-to-override-gethashcode-when-equals-method-is-overridden
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash = (hash * 7) + Abbrv.GetHashCode();
+            return hash;
+        }
+    }
+}
