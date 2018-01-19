4b700cbbf503c42532dcb2af527db82eb604e539
Mike DePouw <mdepouw@ceiamerica.com>
2018-01-19 13:49:09 -0500

    project start

---
+++ Exception Window VS 2017/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //yes I can
+            //throw new Exception("can i bring up the dialog again");
+
+            Task.Run(async () =>
+            {
+                await DoStuff1Async();
+            }).GetAwaiter().GetResult();
+        }
+
+        private static async Task DoStuff1Async()
+        {
+            await DoStuff2Async();
+        }
+
+        private static Task DoStuff2Async()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
