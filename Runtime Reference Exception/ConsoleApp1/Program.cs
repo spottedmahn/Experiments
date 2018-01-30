a15b69b371239e0864c3e22112c1dc345b8072d8
Mike DePouw <mdepouw@ceiamerica.com>
2018-01-30 09:12:31 -0500

    try to repro as described by Satya

---
+++ Runtime Reference Exception/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly.Load("System.Runtime, Version=4.1.1.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+        }
+    }
+}
