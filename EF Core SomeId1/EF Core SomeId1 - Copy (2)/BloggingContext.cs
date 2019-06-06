0f075340d3805e20540b4f3fd99f9a6571dc1653
Mike DePouw <spottedmahn@gmail.com>
2019-06-06 15:40:32 -0400

    Add 3rd copy w/o Manually Defining FK

---
+++ EF Core SomeId1/EF Core SomeId1 - Copy (2)/BloggingContext.cs
@@ -0,0 +1,47 @@
+﻿//https://docs.microsoft.com/en-us/ef/core/get-started/netcore/new-db-sqlite
+
+
+using EF_Core_SomeId1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace ConsoleApp.SQLite
+{
+    public class BloggingContext : DbContext
+    {
+        public static readonly LoggerFactory MyLoggerFactory
+    = new LoggerFactory(new[] { new DebugLoggerProvider() });
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlite("Data Source=blogging.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Id)
+                .HasColumnName("BlogId");
+
+            modelBuilder.Entity<Post>(post =>
+            {
+                post.Property(b => b.Id)
+                .HasColumnName("PostId");
+
+                //post.HasOne(p => p.Blog)
+                //.WithMany()
+                //.HasForeignKey(p => p.BlogId);
+            });
+
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF Core SomeId1/EF Core SomeId1 - Copy (2)/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp.SQLite;
+using EF_Core_SomeId1.Models;
+using System;
+using System.Linq;
+
+namespace EF_Core_SomeId1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteData();
+
+            ReadData();
+        }
+
+        private static void WriteData()
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+
+                db.Posts.Add(new Post
+                {
+                    Blog = db.Blogs.First(),
+                    Title = "Hello World",
+                    Content = "Some content"
+                });
+                db.SaveChanges();
+            }
+        }
+
+        private static void ReadData()
+        {
+            using (var db = new BloggingContext())
+            {
+                var query = db.Blogs
+                                .Select(b => new
+                                {
+                                    b.Id,
+                                    FirstPost = b.Posts.OrderBy(p => p.Id).FirstOrDefault() == null ? null : b.Posts.OrderBy(p => p.Id).FirstOrDefault().Title
+                                });
+
+                var blah = query.ToList();
+                foreach (var rec in blah)
+                {
+                    Console.WriteLine($"{rec.Id} - {rec.FirstPost}");
+                }
+            }
+        }
+    }
+}
