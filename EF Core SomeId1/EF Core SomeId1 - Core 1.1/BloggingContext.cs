c2fa1f09223e9d304450bbd9faa9952b1f715d53
Mike DePouw <spottedmahn@gmail.com>
2019-06-07 09:09:53 -0400

    Try core 1.1

---
+++ EF Core SomeId1/EF Core SomeId1 - Core 1.1/BloggingContext.cs
@@ -0,0 +1,48 @@
+﻿//https://docs.microsoft.com/en-us/ef/core/get-started/netcore/new-db-sqlite
+
+
+using EF_Core_SomeId1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp.SQLite
+{
+    public class BloggingContext : DbContext
+    {
+        public static readonly LoggerFactory MyLoggerFactory
+    = new LoggerFactory();
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            MyLoggerFactory.AddDebug();
+
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlite("Data Source=blogging.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Id)
+                .HasColumnName("BlogId");
+
+            modelBuilder.Entity<Post>(post =>
+            {
+                post.Property(b => b.Id)
+                .HasColumnName("PostId");
+
+                post.HasOne(p => p.Blog)
+                .WithMany()
+                .HasForeignKey(p => p.BlogId);
+            });
+
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF Core SomeId1/EF Core SomeId1 - Core 1.1/Migrations/BloggingContextModelSnapshot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ConsoleApp.SQLite;
+
+namespace EF_Core_SomeId1.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    partial class BloggingContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.6");
+
+            modelBuilder.Entity("EF_Core_SomeId1.Models.Blog", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("BlogId");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("EF_Core_SomeId1.Models.Post", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("PostId");
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<int?>("BlogId1");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("BlogId");
+
+                    b.HasIndex("BlogId1");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("EF_Core_SomeId1.Models.Post", b =>
+                {
+                    b.HasOne("EF_Core_SomeId1.Models.Blog", "Blog")
+                        .WithMany()
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("EF_Core_SomeId1.Models.Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId1");
+                });
+        }
+    }
+}
