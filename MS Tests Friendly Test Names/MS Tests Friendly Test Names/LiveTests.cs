595b116c0efdad1eb2ef9bd8c5fb0bc22b312257
Michael DePouw <mdepouw@ceiamerica.com>
2019-12-18 13:19:39 -0500

    Test MS Test Friendly Test Names (#42)

---
+++ MS Tests Friendly Test Names/MS Tests Friendly Test Names/LiveTests.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MS_Tests_Friendly_Test_Names
+{
+    [TestClass]
+    [TestCategory("Live Unit Tests")]
+    public class LiveTests
+    {
+        [TestMethod()]
+        public void Blah1()
+        {
+            Assert.AreEqual("Hello World", new Class1().HelloWorld());
+        }
+    }
+}
