a6ffe690567d9d032a3227bd433446f15fcfd6f2
Mike DePouw <spottedmahn@gmail.com>
2019-12-18 13:17:39 -0500

    Test MS Test Friendly Test Names

---
+++ MS Tests Friendly Test Names/MS Tests Friendly Test Names/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MS_Tests_Friendly_Test_Names
+{
+    [TestClass]
+    [TestCategory("Testing Display Names")]
+    public class UnitTest1
+    {
+        [TestMethod("Now I can read my test names like a human not a computer 😜")]
+        public void Blah1()
+        {
+        }
+
+        [TestMethod("Can VS please implement 'proper emojis'? 😜 fdfsdf")]
+        public void Blah2()
+        {
+        }
+    }
+}
