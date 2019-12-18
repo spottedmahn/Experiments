9c020ecce8414ec284f422c61a585257b70f6844
Michael DePouw <mdepouw@ceiamerica.com>
2019-12-18 11:13:16 -0500

    No clue random stuff (#33)
    * Integration Testing w/ SignalR and HubContext
    
    * No clue, random stuff

---
+++ Rename ConsoleApp Issue/Rename ConsoleApp Issue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rename_ConsoleApp_Issue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Worker Test/Worker Test/NoBuildAction.cs
@@ -0,0 +1,7 @@
+﻿namespace Worker_Test
+{
+    public class NoBuildAction
+    {
+        some bad code
+    }
+}
